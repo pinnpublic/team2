2ec648d2237e5fc7c63c9fdf27b95387ce92f1ae
pinn.public <pinn.public@gmail.com>
2025-11-11 12:04:28 +0900

    Add 78

--- script.sql
+++ script.sql
@@ -1,9 +1,9 @@
--- [추가] 공식 이미지는 유저를 자동 생성하지 않으므로 직접 생성합니다.
+
 CREATE USER springboot IDENTIFIED BY java1234;
 GRANT CONNECT, RESOURCE TO springboot;
 ALTER USER springboot DEFAULT TABLESPACE USERS QUOTA UNLIMITED ON USERS;
 
--- [수정] 이제 springboot 유저 소유로 테이블을 생성합니다.
+
 create table springboot.tblTeam (
     seq number primary key,
     name varchar2(100) not null unique,
